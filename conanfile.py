5f96a09f80f4c329b5e1b93517a346f49044c498
tt4g <mainlinett@gmail.com>
2019-09-24 05:49:44 +0900

    Bump patch version to 5.11.2

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ from conans import ConanFile, tools
 
 class ConanfontawesomedesktopConan(ConanFile):
     name = "Font-Awesome-Desktop"
-    version = "5.11.1"
+    version = "5.11.2"
     settings = None
     description = "The iconic SVG, font, and CSS toolkit https://fontawesome.com"
     url = "https://github.com/tt4g/conan-Font-Awesome-Desktop"
@@ -20,7 +20,7 @@ class ConanfontawesomedesktopConan(ConanFile):
 
     def source(self):
         extracted_dir = "fontawesome-free-{0}-desktop".format(self.version)
-        sha256 = "971cea4af2b57b214994a03e3ed69a32e65b5fa2146efa88f289e962d118abcc"
+        sha256 = "5c5fe6c31ed5ccb0c7b959a4f160501b7c2377e2aef2d5370f645f924db097b8"
         tools.get("{0}/releases/download/{1}/{2}.zip".format(self.homepage, self.version, extracted_dir),
                   sha256=sha256)
         os.rename(extracted_dir, self._source_subfolder)
