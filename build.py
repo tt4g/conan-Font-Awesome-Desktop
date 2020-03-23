c2f13510f270a0af56fe3817d2b15baa97ec866c
tt4g <mainlinett@gmail.com>
2020-03-24 05:55:22 +0900

    Update CI tests

--- build.py
+++ build.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from cpt.packager import ConanMultiPackager
-
+from bincrafters import build_template_default
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
-    builder.add_common_builds()
+
+    builder = build_template_default.get_builder(pure_c=False)
     builder.run()
